7b1ab28cb348423d879eb0188bd88b20e83b0e22
zhangwei <396033084@qq.com>
2016-03-22 20:27:06 +0800

    test

--- src/test/java/com/tmsps/ne4SpringBoot/SpringService.java
+++ src/test/java/com/tmsps/ne4SpringBoot/SpringService.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Service;
 public class SpringService implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
  
-    @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
     	SpringService.applicationContext = applicationContext;
     }
