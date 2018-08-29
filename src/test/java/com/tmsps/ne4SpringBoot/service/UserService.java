6a7026829b0d4fd1a903b468c2cdb6c5ce6ef8b6
zhangwei <396033084@qq.com>
2018-08-29 08:36:29 +0800

    update spring boot to 2.0.4.RELEASE update ne4Spring to 5.0.8

--- src/test/java/com/tmsps/ne4SpringBoot/service/UserService.java
+++ src/test/java/com/tmsps/ne4SpringBoot/service/UserService.java
@@ -6,16 +6,17 @@ import java.util.Map;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSON;
+import com.tmsps.ne4spring.base.BaseMySQLService;
 
 
 
 @Service
-public class UserService extends BaseService{
+public class UserService extends BaseMySQLService{
 	
 	
 	public String say(){
 		final String sql = "SELECT * FROM wx_user t order by t.subscribe_time desc";
-		List<Map<String, Object>> list = baseMySQLService.findList(sql);
+		List<Map<String, Object>> list = this.findList(sql);
 		return JSON.toJSONString(list);
 	}
 }
